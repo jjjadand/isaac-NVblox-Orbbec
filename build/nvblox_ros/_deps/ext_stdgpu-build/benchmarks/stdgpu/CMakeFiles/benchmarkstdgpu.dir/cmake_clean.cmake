f1032d53e468caf1d59dc5207e2592257fdac4f9
darklee <2334721338@qq.com>
2026-01-21 13:51:44 +0800

    add build

---
+++ build/nvblox_ros/_deps/ext_stdgpu-build/benchmarks/stdgpu/CMakeFiles/benchmarkstdgpu.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "../../bin/benchmarkstdgpu"
+  "../../bin/benchmarkstdgpu.pdb"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/bitset.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/bitset.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/deque.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/deque.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/mutex.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/mutex.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/unordered_map.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/unordered_map.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/unordered_set.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/unordered_set.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/vector.cu.o"
+  "CMakeFiles/benchmarkstdgpu.dir/cuda/vector.cu.o.d"
+  "CMakeFiles/benchmarkstdgpu.dir/main.cpp.o"
+  "CMakeFiles/benchmarkstdgpu.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/benchmarkstdgpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
